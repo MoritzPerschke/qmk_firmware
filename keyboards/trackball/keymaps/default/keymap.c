9b146a59f38c703293808c5e0afc19e48b6923ef
Moritz Perschke <moritz.perschke@student.uibk.ac.at>
2025-01-24 08:14:43 +0100

    added scrolling to trackball

--- keyboards/trackball/keymaps/default/keymap.c
+++ keyboards/trackball/keymaps/default/keymap.c
@@ -1,4 +1,7 @@
+#include <stdint.h>
+#include "config.h"
 #include QMK_KEYBOARD_H
+
 #if __has_include("keymap.h")
 #    include "keymap.h"
 #endif
@@ -11,10 +14,10 @@ enum custom_layer {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [MOUSE] = LAYOUT(
-        KC_BTN1, LT(ALT, KC_BTN2), KC_BTN3, KC_WH_U, KC_WH_D
+        KC_BTN1, LT(ALT, KC_BTN2), KC_BTN3, KC_WH_D, KC_WH_U
     ),
     [ALT] = LAYOUT(
-        KC_BTN1, KC_BTN2, KC_ACL0, KC_ACL1, KC_ACL2
+        KC_BTN1, KC_BTN2, KC_BTN4, KC_BTN5, KC_A
     )
 };
 
@@ -24,3 +27,24 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 
+// https://docs.qmk.fm/features/pointing_device#advanced-drag-scroll
+float scroll_accumulated_h = 0;
+float scroll_accumulated_v = 0;
+
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    // Check if SCROLL layer is active
+    if (IS_LAYER_ON(ALT)) {
+        scroll_accumulated_h += (float)mouse_report.x / SCROLL_DIVISOR_H;
+        scroll_accumulated_v += (float)mouse_report.x / SCROLL_DIVISOR_V;
+
+        mouse_report.h = (int8_t)scroll_accumulated_h;
+        mouse_report.v = (int8_t)scroll_accumulated_v;
+
+        scroll_accumulated_h -= (int8_t)scroll_accumulated_h;
+        scroll_accumulated_v -= (int8_t)scroll_accumulated_v;
+
+        mouse_report.x = 0;
+        mouse_report.y = 0;
+    }
+    return mouse_report; // Pass the modified report to QMK for further processing
+}
