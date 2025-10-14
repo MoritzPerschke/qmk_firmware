7c3a5d04b2179fa3395919608d19f5eccb469768
Moritz Perschke <perschkemoritz@gmail.com>
2025-10-15 00:58:42 +0200

    now only thing left is defining the keymap and trackball behaviour

--- keyboards/v2/keymaps/default/keymap.c
+++ keyboards/v2/keymaps/default/keymap.c
@@ -8,18 +8,30 @@
 
 enum custom_layer {
     BASE,
+    NAV,
+    SYM,
+    FUNC,
+    BLT,
     MOUSE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BASE] = LAYOUT(
-        KC_GRV,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,     KC_O,   KC_P,         KC_BSLS,
-        KC_LSFT, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,     KC_L,   KC_SEMICOLON, KC_RSFT,
-        KC_LCTL, KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMMA, KC_DOT, KC_SLASH,     KC_RCTL,
-                                  KC_DEL,  KC_ENT,  KC_TAB,      KC_BSPC, KC_SPC,  KC_QUOT,
-		                      	  KC_LCTL, KC_LALT
+        KC_GRV,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,    /**/    KC_Y,    KC_U,    KC_I,     KC_O,   KC_P,         KC_BSLS,
+        KC_LSFT, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,    /**/    KC_H,    KC_J,    KC_K,     KC_L,   KC_SEMICOLON, KC_RSFT,
+        KC_LCTL, KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,    /**/    KC_N,    KC_M,    KC_COMMA, KC_DOT, KC_SLASH,     KC_RCTL,
+                                  KC_DEL,  KC_ENT,  KC_TAB,  /**/    KC_BSPC, KC_SPC,  KC_QUOT,
+		                      	  KC_LGUI, KC_LALT
     ),
+
+    [NAV] = LAYOUT(
+        KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,    /**/    KC_TRANS, KC_TRANS,  KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,
+        KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,    /**/    KC_TRANS, KC_TRANS,  KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,
+        KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,    /**/    KC_TRANS, KC_TRANS,  KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,
+                                      KC_TRANS, KC_TRANS, KC_TRANS,    /**/    KC_TRANS, KC_TRANS,  KC_TRANS,
+		                      	      KC_TRANS, KC_TRANS
+    )
 };
 
 // https://docs.qmk.fm/features/pointing_device#advanced-drag-scroll
